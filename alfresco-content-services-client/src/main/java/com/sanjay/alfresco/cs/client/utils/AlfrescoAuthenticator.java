f4e6353ac781bca92adc9f7700c17fb6e1a0c418
Gokul Alok Vardhan Sanjay Chintala <89155561+sanjaychgav@users.noreply.github.com>
2023-12-13 05:56:15 +0000

    AlfAuth

---
+++ alfresco-content-services-client/src/main/java/com/sanjay/alfresco/cs/client/utils/AlfrescoAuthenticator.java
@@ -0,0 +1,21 @@
+package com.sanjay.alfresco.cs.client.utils;
+
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.sanjay.alfresco.cs.client.constants.Constants;
+
+import jakarta.annotation.PreDestroy;
+
+
+@Component
+public class AlfrescoAuthenticator{
+
+}
