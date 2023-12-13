078192bb841b135bd435828fa822ccc5ba3fcd9b
Gokul Alok Vardhan Sanjay Chintala <89155561+sanjaychgav@users.noreply.github.com>
2023-12-13 04:29:07 +0000

    Maven prj

---
+++ alfresco-content-services-client/src/main/java/com/sanjay/alfresco/cs/client/Application.java
@@ -0,0 +1,13 @@
+package com.sanjay;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
