e07e7b93b9b11f286f1c6b9f8445fee597ba30df
Gokul Alok Vardhan Sanjay Chintala <89155561+sanjaychgav@users.noreply.github.com>
2023-12-13 05:16:25 +0000

    constansts

---
+++ alfresco-content-services-client/src/main/java/com/sanjay/alfresco/cs/client/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.sanjay.alfresco.cs.client.constants;
+
+public interface Constants{
+ 
+    public static final String PATH_AUTHENTICATION = "authentication/version/1/tickets";
+    public static final String PATH_NODES = "alfresco/versions/1/nodes";
+}
