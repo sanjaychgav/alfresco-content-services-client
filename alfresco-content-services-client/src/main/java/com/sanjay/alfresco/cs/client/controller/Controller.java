081b8c0be414959a91209c06836639ff6fff04ce
Gokul Alok Vardhan Sanjay Chintala <89155561+sanjaychgav@users.noreply.github.com>
2023-12-13 07:06:44 +0000

    typo

--- alfresco-content-services-client/src/main/java/com/sanjay/alfresco/cs/client/controller/Controller.java
+++ alfresco-content-services-client/src/main/java/com/sanjay/alfresco/cs/client/controller/Controller.java
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.sanjay.alfresco.cs.client.model.FileMetadata;;
+import com.sanjay.alfresco.cs.client.model.FileMetadata;
 // import com.sanjay.alfresco.cs.client.service.NodesService;
-import com.sanjay.alfresco.cs.client.utils.AlfrescoAuthenticator;;
+import com.sanjay.alfresco.cs.client.utils.AlfrescoAuthenticator;
 import com.sanjay.alfresco.cs.client.utils.Commons;
 
 import lombok.extern.slf4j.Slf4j;
